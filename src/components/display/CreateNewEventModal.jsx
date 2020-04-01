3a751731820df17a8be2de57bdbe868590da456e
Levente Fodor <levilevi183@gmail.com>
2020-04-01 14:55:47 +0200

    fix typo in button component

--- src/components/display/CreateNewEventModal.jsx
+++ src/components/display/CreateNewEventModal.jsx
@@ -106,7 +106,7 @@ export default function CreateNewEventModal() {
                                 >
                                 Attach Images
                                 <input
-                                    type="files"
+                                    type="file"
                                     style={{ display: "none" }}
                                 />
                             </Button>
