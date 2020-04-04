1de0a0cee25af107578f1fdb66155e01784e8178
Levente Fodor <levilevi183@gmail.com>
2020-04-04 19:49:54 +0200

    Add positional styling for NumberOfThanksCard

--- src/components/cards/Badges/NumberOfThanksCard.jsx
+++ src/components/cards/Badges/NumberOfThanksCard.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import { styles } from '../../../style';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -8,6 +7,10 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 const useStyles = makeStyles({
   card: {
     color: styles.inherit,
+  },
+  cardContainer: {
+    float: 'left',
+    paddingLeft: 10
   }
 });
 
@@ -17,7 +20,7 @@ export default function NumberOfThanksCard() {
 
   return (
     <div>
-        <Typography variant="h5" align="center" component="h2">
+        <Typography className={classes.cardContainer} variant="h5" align="center" component="h2">
             <FavoriteIcon className={classes.card} />  15 
         </Typography> 
     </div>
